f27a07cfc60a0d0beb225235f6966cb5b5b49f23
Prabu <46627064+prabuk2012@users.noreply.github.com>
2022-07-05 21:30:09 +0530

    env variables in prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseUrl: 'https://wpupcabensdlhvtrxhey.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndwdXBjYWJlbnNkbGh2dHJ4aGV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTMzODcyODksImV4cCI6MTk2ODk2MzI4OX0.SsG0ptSI8IH7P3_KhLDJCPQp5LQO-QgD2pQftMb8RP4'
 };
